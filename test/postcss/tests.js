4a714a56bac27a72a98faa81a5a76540900a5060
Martijn Cuppens <martijn.cuppens@intracto.com>
2018-12-25 14:07:04 +0100

    Mocha tests

---
+++ test/postcss/tests.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'test-1': {},
+  'test-2': {
+    enableResponsiveFontSizes: false
+  },
+  'test-3': {
+    class: 'disable'
+  },
+  'test-4': {
+    class: 'enable'
+  }
+};
