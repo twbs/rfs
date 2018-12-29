0c22ee2a5073e7265d1008179c58c97e43d3a4c7
Martijn Cuppens <martijn.cuppens@intracto.com>
2018-12-29 01:16:27 +0100

    More tests

--- test/lib/tests.js
+++ test/lib/tests.js
@@ -15,4 +15,28 @@ module.exports = [
     id: 'test-4',
     name: 'Enable class'
   },
+  {
+    id: 'test-5',
+    name: 'Change breakpoint unit to em'
+  },
+  {
+    id: 'test-6',
+    name: 'Change base font size'
+  },
+  {
+    id: 'test-7',
+    name: 'Change font size unit'
+  },
+  {
+    id: 'test-8',
+    name: 'Enable two dimensional'
+  },
+  {
+    id: 'test-9',
+    name: 'Go loco with the config'
+  },
+  {
+    id: 'test-10',
+    name: 'Include mixins multiple times (Should throw a warning for SCSS & Stylus and not fail for less & PostCSS)'
+  },
 ];
