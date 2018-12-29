218c6f01b16380d6d80d69a4098fc0ebac8d1a84
Martijn Cuppens <martijn.cuppens@gmail.com>
2018-12-29 01:22:16 +0100

    More mocha tests (#62)

--- test/postcss/tests.js
+++ test/postcss/tests.js
@@ -8,5 +8,37 @@ module.exports = {
   },
   'test-4': {
     class: 'enable'
+  },
+  'test-5': {
+    breakpointUnit: 'em'
+  },
+  'test-6': {
+    baseFontSize: '17px'
+  },
+  'test-7': {
+    fontSizeUnit: 'px'
+  },
+  'test-8': {
+    twoDimensional: true
+  },
+  'test-9': {
+    baseFontSize: '12px',
+    fontSizeUnit: 'px',
+    breakpoint: 800,
+    breakpointUnit: 'rem',
+    twoDimensional: true,
+    factor: 5,
+    class: true,
+    safariIframeResizeBugFix: true,
+  },
+  'test-10': { // Not testable
+    baseFontSize: '12px',
+    fontSizeUnit: 'px',
+    breakpoint: 800,
+    breakpointUnit: 'rem',
+    twoDimensional: true,
+    factor: 5,
+    class: true,
+    safariIframeResizeBugFix: true,
   }
 };
