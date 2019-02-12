911a47e731a0fae6b19489959ea5d3a75bd2d01c
XhmikosR <xhmikosr@gmail.com>
2019-02-12 21:17:55 +0200

    Lint test.js.

--- test/test.js
+++ test/test.js
@@ -1,29 +1,31 @@
-const assert = require('assert');
-const tests = require("./lib/tests.js");
-const styles = ['Less', 'Sass', 'Stylus', 'PostCSS'];
-const result = require("./lib/result.js");
+'use strict';
 
-styles.forEach((style) => {
-  describe(style, function () {
-    tests.forEach((test) => {
-      it(test.name, function (done) {
+const assert = require('assert');
+const tests = require('./lib/tests.js');
+
+const styles = ['Less', 'Sass', 'Stylus', 'PostCSS'];
+const result = require('./lib/result.js');
+
+styles.forEach(style => {
+  describe(style, () => {
+    tests.forEach(test => {
+      it(test.name, done => {
         const generated = result[style.toLowerCase()](test.id);
         const expected = result.expected(test.id);
+
         // If promise:
         if (typeof generated.then === 'function') {
-          generated.then((generated) => {
-            assert.equal(generated, expected);
+          generated.then(generated => {
+            assert.strictEqual(generated, expected);
             done();
-          }).catch((error) => {
+          }).catch(error => {
             done(error);
           });
-        }
-        else {
-          assert.equal(generated, expected);
+        } else {
+          assert.strictEqual(generated, expected);
           done();
         }
       });
     });
   });
 });
-
